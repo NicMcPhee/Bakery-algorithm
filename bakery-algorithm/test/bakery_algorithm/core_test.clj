715ab763f4aa70ff408618a972ad91ede182837a
Nic McPhee <mcphee@morris.umn.edu>
2013-02-14 18:22:29 -0600

    Added a test that checks that ticket numbers are unique even when request in multiple threads in parallel.

--- bakery-algorithm/test/bakery_algorithm/core_test.clj
+++ bakery-algorithm/test/bakery_algorithm/core_test.clj
@@ -21,13 +21,14 @@
 
 (deftest ticket-machine-test
   (testing "Ticket increments on each call"
-    (reset! ticket-machine 0)
+    (reset! ticket-machine -1)
     (is (= 0 (next-ticket)))
     (is (= 1 (next-ticket)))
     (is (= 2 (next-ticket))))
-  (testing "Tickets are unique and in order with threads"
-    (reset! ticket-machine 0)
-    (wait-futures 10 (next-ticket))
+  (testing "Tickets are unique with threads"
+    (reset! ticket-machine -1)
+    (is (= (range 20)
+           (sort (pmap (fn [_] (next-ticket)) (range 20)))))
     ))
 
 (ticket-machine-test)
