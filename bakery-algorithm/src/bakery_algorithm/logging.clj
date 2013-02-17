d35a25cc7b996864dc127b84849d53fdd411a149
Nic McPhee <mcphee@morris.umn.edu>
2013-02-17 11:11:48 -0600

    Implemented a basic write behind logger taking pretty much directly from Chapter 4 of Programming Clojure by Emerick, et al.

---
+++ bakery-algorithm/src/bakery_algorithm/logging.clj
@@ -0,0 +1,20 @@
+(ns bakery-algorithm.logging)
+
+(require '[clojure.java.io :as io])
+
+(def console (agent *out*))
+
+(defn write
+  [^java.io.Writer w & content]
+  (doseq [x (interpose "" content)]
+    (.write w (str x)))
+  (doto w
+    (.write "\n")
+    .flush))
+
+(defn log-reference
+  [reference & writer-agents]
+  (add-watch reference :log
+             (fn [_ reference old new]
+               (doseq [writer-agent writer-agents]
+                 (send-off writer-agent write new)))))
